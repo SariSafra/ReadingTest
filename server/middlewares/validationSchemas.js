470804b3b48c93558c08b62ef8496606a1afa13f
Sari Safra <s0504175747@gmail.com>
2024-06-22 23:23:35 +0300

    teacher validation in client and server

---
+++ server/middlewares/validationSchemas.js
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+// Define the schema for signup validation
+export const signupSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required()
+});
+
+// Define the schema for email validation
+export const emailSchema = Joi.object({
+  email: Joi.string().email().required()
+});
+
+// Middleware to validate the request body against the provided schema
+export const validateRequest = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    console.log("validation error in middlware")
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
