5ca5bfb6bd126ee0a09aa442bd7768b0406af74a
chana-rozin <c0556787069@gmail.com>
2024-06-30 00:35:01 +0300

    :)

--- client/client/src/pages/teacherArea/ShowStudentDiagnosis.jsx
+++ client/client/src/pages/teacherArea/ShowStudentDiagnosis.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { getStudentDiagnoses } from '../../services/api'; 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import DeleteDiagnosis from './DeleteDiagnosis';
